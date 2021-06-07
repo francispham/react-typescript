3c338e7a7564b730e131cd797f17c0ef7dde301e
Francis Pham <32145268+francispham@users.noreply.github.com>
2021-06-07 14:09:45 -0700

    Introduced Typing useRef with Typing DOM Elements, Strick Null Checking & Optional Chaining

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 export const Inputs = () => {
   const [name, setName] = useState('');
   const [age, setAge] = useState<string>('');
   const [address, setAddress] = useState<string | null>(null);
 
+  const ref = useRef<HTMLInputElement>(null!);            // null! is read only!
+  /* Same as above but not commonly used!
+    const ref = useRef<HTMLInputElement | null>(null);     
+  */
+
+  console.log('ref:', ref?.current?.value);
+
   return (
     <>
-      <input value={name} onChange={e => setName(e.target.value)} />
+      <input ref={ref} value={name} onChange={e => setName(e.target.value)} />
       <input value={age} onChange={e => setAge(e.target.value)} />
       <input value={address === null ? '' : address} onChange={e => setAddress(e.target.value)} />
     </>
