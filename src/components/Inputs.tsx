e4d6b21d3920eccc9c0e5c98303fe98a0897297a
Francis Pham <francis.pham.ca@gmail.com>
2025-12-14 09:41:25 -0800

    fix lint error

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -1,25 +1,17 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 export const Inputs = () => {
   const [name, setName] = useState("");
   const [age, setAge] = useState<string>("");
   const [address, setAddress] = useState<string | null>(null);
 
-  const ref = useRef<HTMLInputElement>(null!); // null! is read only!
-  /* Same as above but not commonly used!
-    const ref = useRef<HTMLInputElement | null>(null);     
-  */
-
-  console.log("HTMLInputElement: ", ref?.current?.value);
-
   return (
-    <section className="p-3 pl-0" ref={ref}>
+    <section className="p-3 pl-0">
       <div className="flex items-center m-2 w-md rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
         <input
           type="text"
           className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
           placeholder="Name with ref"
-          ref={ref}
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
